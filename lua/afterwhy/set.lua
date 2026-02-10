621164508520ab08823a1ac9f92fd5b6c9424454
afterwhy <afterwhy@gmail.com>
2026-02-10 22:55:59 +0300

    feat(opts): option changes to check is they're better

--- lua/afterwhy/set.lua
+++ lua/afterwhy/set.lua
@@ -27,12 +27,12 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 --remove tildas
-vim.opt.fillchars = vim.opt.fillchars + "eob: "
+-- vim.opt.fillchars = vim.opt.fillchars + "eob: "
 
 --vim.opt.guicursor = " "
 
 vim.opt.nu = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -51,16 +51,16 @@ vim.opt.wrap = false
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
---vim.opt.termguicolors = true
+vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+-- vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "100"
+vim.opt.colorcolumn = ""
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
